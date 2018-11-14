00d6d0717abc5b8d6cef833fd0c1c7be43f73684
Jan Sandbrink <nobodysnightmare@gmail.com>
2018-11-14 08:06:22 +0100

    Order category selects alphabetically

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@
 
 module ApplicationHelper
   def category_options(selected = nil)
-    categories = Category.all
+    categories = Category.order(name: :asc)
     no_category = [[I18n.t('no_category'), '']]
     options_for_select(
       no_category + categories.map { |c| [c.name, c.id] },
